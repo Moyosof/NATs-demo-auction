69341b90ca0b3a2019a598a5e66633e099b34723
moyosof <iamthaizzie@gmail.com>
2024-02-09 09:17:58 +0100

    added request and reply

--- NatsDemo.Api/NatsPublisherService.cs
+++ NatsDemo.Api/NatsPublisherService.cs
@@ -17,16 +17,40 @@ namespace NatsDemo.Api
         /// </summary>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
+        //public Task StartAsync(CancellationToken cancellationToken)
+        //{
+        //    var subscription = _natsConnection.SubscribeAsync("auction.created");
+        //    subscription.MessageHandler += (sender, args) =>
+        //    {
+        //        var message = Encoding.UTF8.GetString(args.Message.Data);
+        //        Console.WriteLine($"Publish message: {message}");
+        //    };
+        //    subscription.Start();
+        //    Console.WriteLine("Publish 'auction.created'. Waiting to publish messages...");
+
+        //    return Task.CompletedTask;
+        //}
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            var subscription = _natsConnection.SubscribeAsync("auction.created");
-            subscription.MessageHandler += (sender, args) =>
+            // Subscription for "auction.created" messages
+            var createdSubscription = _natsConnection.SubscribeAsync("auction.created");
+            createdSubscription.MessageHandler += (sender, args) =>
             {
                 var message = Encoding.UTF8.GetString(args.Message.Data);
                 Console.WriteLine($"Publish message: {message}");
             };
-            subscription.Start();
-            Console.WriteLine("Publish 'auction.created'. Waiting to publish messages...");
+            createdSubscription.Start();
+
+            // Subscription for "auction.response" messages
+            var responseSubscription = _natsConnection.SubscribeAsync("auction.response");
+            responseSubscription.MessageHandler += (sender, args) =>
+            {
+                var message = Encoding.UTF8.GetString(args.Message.Data);
+                Console.WriteLine($"Received auction response: {message}");
+            };
+            responseSubscription.Start();
+
+            Console.WriteLine("Subscribed to 'auction.created' and 'auction.response'. Waiting for messages...");
 
             return Task.CompletedTask;
         }
